bfcd4ebbcc458f49ed39a0477f91feafe31db914
SonkayAugustine <zamanisonkay@gmail.com>
2022-01-31 06:38:05 -0800

    fixed task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,12 +7,19 @@ import requests
 from sys import argv
 
 if __name__ == ' __main__':
-        userId = argv[1]
-        req_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId), verify=False).json()
-        to_do = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(userId), verify=False).json()
-        completed_tasks = []
-        for task in to_do:
-                if task.get("completed") is True:
-                    completed_tasks.append(task.get('title'))
-        print("Employee {} is done with tasks({}/{}):".format(req_user.get('name'), len(completed_tasks), len(to_do)))
-        print("\n".join("\t {}".format(task) for task in completed_tasks))
+        if len(argv) > 1:
+                user = argv[1]
+                api_url = "https://jsonplaceholder.typicode.com/"
+                req_user = requests.get("{}users/{}".format(api_url, user))
+                name = req_user.json().get('name')        
+                if name is not None:
+                        to_do = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(url, user)).json()
+                        all_task = len(to_do)
+                        completed_tasks = []
+                        for task in to_do:
+                                if task.get("completed") is True:
+                                        completed_tasks.append(task)
+                        completed_count = len(completed_tasks)
+                        print("Employee {} is done with tasks({}/{}):".format(name, completed_count, all_task))
+                        for title in completed_tasks:
+                                print("\t {}".format(title.get("title")))
