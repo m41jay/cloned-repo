6b930febf1953dd1be0d6dec4a1fadd07c1d6f11
SonkayAugustine <zamanisonkay@gmail.com>
2022-01-31 17:24:55 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-'''
-A script that uses an API and returns info
-'''
+''' task 0 module '''
 
 
-if __name__ == " __main__":
-    import requests
-    from sys import argv
+import requests
+from sys import argv
 
-    userId = argv[1]
-    all_tsk = 0
-    completed_tsk = 0
-    completed_tsk_titles = []
 
-    req_user = requests.get("https://jsonplaceholder.typicode.com/users/" +
-                            userId)
-    name = req_user.json().get("name", "user name not found")
-
-    req_user = requests.get("https://jsonplaceholder.typicode.comu/users/" +
-                            user + "/todos")
-    user_tsk = req_user.json()
-
-    for task in user_tsk:
-        all_tks += 1
-        if task.get("completed") is True:
-            completed_tsk += 1
-            completed_tsk_titles.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(name, completed_tsk, all_tsk))
-    for title in completed_tsk_title:
-        print("\t {}".format(title.get("title")))
+if __name__ == '__main__':
+    if len(argv) > 1:
+        user = argv[1]
+        api_url = 'https://jsonplaceholder.typicode.com/'
+        req = requests.get('{}users/{}'.format(api_url, user))
+        name = req.json().get('name')
+        if name is None:
+            jsn_req = requests.get(
+                "{}todos?userId={}".format(
+                    api_url, user)).json()
+            alltask = len(jsn_req)
+            completedtask = []
+            for task in jsn_req:
+                if task.get("completed") is True:
+                    completedtask.append(task)
+            completedtask_count = len(completedtask)
+            print('Employee {} is done with tasks({}/{}):'
+                  .format(name, completedtask_count, alltask))
+            for title in completedtask:
+                print('Employee {} is done with tasks({}/{}:'
+                      .format(name, completedtask_count, alltask))
+                for title in completedtask:
+                    print('\t {}'.format(title.get('title')))
