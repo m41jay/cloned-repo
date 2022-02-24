05f8548361cee02e0d3ff02df3dc1cb16bd7a022
SonkayAugustine <zamanisonkay@gmail.com>
2022-02-24 21:23:37 +0000

    fixed t0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Update limit in the /etc/default/nginx file uaing Puppet
 exec { 'update_ulimit':
-  command => 'sed -i \'s/15/15000/\' /etc/default/nginx && service nginx restart',
-  path    => [ '/bin/', /sbin/', '/usr/bin/', '/usr/sbin/' ]
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"/g" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin/:/usr/local/bin/:/usr/sbin:/usr/bin:/sbin:/bin',
 }
